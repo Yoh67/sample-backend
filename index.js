0e70852f4b061861b92216701124170a5af70049
Yoh <yoseph.solomon67@gmail.com>
2019-10-23 23:27:47 -0700

    Successfully implemented routers, moving on to building out the rest of the CRUD apis

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 const express = require('express'); // Initialize Express..
 const {database} = require('./model/database_config.js'); // Initialize database..
 
-const testRouter = require('./routers/router.js');
+const routerEndpoints = require('./routers/router.js');
 
 const app = express();
 const port = 3000;
 
 const router = express.Router();
 
-app.use('/api', router);
-router.get('/users', testRouter.test);
-
 // Startup port, log to console
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`));
+
+app.use('/api', router);
+
+router.get('/users', routerEndpoints.getAllUsers);
+router.get('/users/:id', routerEndpoints.getSingleUser);
+//router.post('/users/:id', routerEndpoints.createSingleUser);
 
 // Home/splash page will return Hello World!
 app.get('/', (request, response) => {
     response.send('sample-backend Hello World!')
 });
 
-// GET @ custom endpoint, then we propagate the query results
-app.get('/read/:id', (request, response) => {
-    database.any('SELECT * FROM sample_table WHERE user_id = $1', request.params.id)
-        .then(data => {
-            response.send('GET successful! \nData: ' + 
-                JSON.stringify(data)
-                );
-        })
-        .catch(error => {
-            response.send(error);
-        });
-});
-
-// GET @ custom endpoint, then we cache into our database
-app.get('/insert/:id', (request, response) => {
+// POST insert user
+app.post('/insert/:id', function (request, response) {
     database.one('INSERT INTO sample_table(user_id, message) VALUES($1, $2) RETURNING user_id', 
     [request.params.id, 'Auto Insert '.concat(request.originalUrl)])
         .then(data => {
@@ -44,8 +34,4 @@ app.get('/insert/:id', (request, response) => {
         .catch(error => {
             response.send(error);
         });
-});
-
-app.post('/', function (request, response) {
-    response.send("POST: " + request.originalUrl);
 });
--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,8 @@
 const {database} = require('../model/database_config.js'); // Initialize database..
 
-module.exports.test = function (request, response) {
-    database.any('SELECT * FROM sample_table WHERE user_id = $1', request.params.id)
+// Get All
+module.exports.getAllUsers = function (request, response) {
+    database.any('SELECT * FROM sample_table')
         .then(data => {
             response.send('GET successful! \nData: ' + 
                 JSON.stringify(data)
@@ -9,22 +10,18 @@ module.exports.test = function (request, response) {
     })
     .catch(error => {
         console.log("Error");
-        response.send(error);
+    });
+}
+
+// Get Single User
+module.exports.getSingleUser = function (request, response) {
+    database.any('SELECT * FROM sample_table WHERE user_id = $1', request.params.id)
+        .then(data => {
+            response.send('GET successful! \nData: ' + 
+                JSON.stringify(data)
+            );
+    })
+    .catch(error => {
+        console.log('Error:', error);
     });
 };
-
-// const router = express.Router();
-
-// const handlerFn = function (request, response) {
-//     if (request.query) {
-//         const test = `SELECT * FROM sample_table`.execute();
-
-//         return response.status(200).JSON(test);
-//     } else {
-//         return response.status(400).JSON(test);
-//     }
-// }
-
-// // Return all users
-// router.get('/users', handlerFn);
-// app.use('/api', router);
