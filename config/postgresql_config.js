25c38d920b94376216a6246d9d36d9823af4178c
Yoh <yoseph.solomon67@gmail.com>
2020-09-27 13:10:41 -0700

    v1.2 Initial commit, added MIT License, added necessary project details

--- config/postgresql_config.js
+++ config/postgresql_config.js
@@ -1,11 +1,13 @@
 const bluebird = require('bluebird'); 
 const monitor = require('pg-monitor');
 
-// Initialize pg-promsie with options..
 const initOptions = { 
     /// Designate bluebird as promise library
     promiseLib: bluebird,
 };
+
+// Initialize pg-promise
+const pgp = require('pg-promise')(initOptions);
 
 // Attach pgmonitor
 monitor.attach(initOptions, [
@@ -14,9 +16,6 @@ monitor.attach(initOptions, [
     'error'
 ]);
 monitor.setTheme('matrix');
-
-// Initialize pg-promise
-const pgp = require('pg-promise')(initOptions);
 
 // Establish database connection
 const database = pgp(process.env.DATABASE_URL || process.env.HEROKU_POSTGRESQL_BLUE_URL);
