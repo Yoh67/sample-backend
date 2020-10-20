9332ff633494c384af1a6b9d9b6db0de76662918
Yoh <yoseph.solomon67@gmail.com>
2020-10-19 22:11:57 -0700

    Appears to be the first working example of basic auth using passport, needs more dev testing

--- src/utils/authorization_util.ts
+++ src/utils/authorization_util.ts
@@ -1,22 +1,39 @@
 import passport, { Strategy } from 'passport';
 import {BasicStrategy} from 'passport-http';
 
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+const users = [
+    { username: clientId, password: clientSecret }
+];
+
 function VerifyUser(username: any, callback: any) {
-    if (username === process.env.CLIENT_ID) {
-        return callback(null, username)
-    } else {
+    process.nextTick(() => {
+        for (let i = 0, length = users.length; i < length; i++) {
+            const userRecord = users[i];
+            if (username === userRecord.username) {
+                return callback(null, userRecord)
+            }
+        }
         return callback(null, null);
-    }
-}
+    });
+};
 
 export function InitializeAuthorization() {
     passport.use(new BasicStrategy(
+        {},
         (username, password, done) => {
-            VerifyUser({ username }, (error: any, user: any) => {
-            if (error) { return done(error); }
-            if (!user) { return done(null, false); }
-            if (user.password !== password) { return done(null, false); }
-            return done(null, user);
+            VerifyUser(username, (error: any, user: any) => {
+                console.log(
+                    username + '\n'+
+                    user + '\n' +
+                    user.password + '\n' +
+                    password);
+                if (error) { return done(error); }
+                if (!user) { return done(null, false); }
+                if (user.password !== password) { return done(null, false); }
+                return done(null, user);
           });
         }
       ));
